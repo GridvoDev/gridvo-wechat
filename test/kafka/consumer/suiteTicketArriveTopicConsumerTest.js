9841e79955604113c72e87d69eb25909da6651f7
Johnlinmadan <772181827@qq.com>
2016-10-08 11:10:58 +0800

    添加kafka单元测试代码

--- test/kafka/consumer/suiteTicketArriveTopicConsumerTest.js
+++ test/kafka/consumer/suiteTicketArriveTopicConsumerTest.js
@@ -16,57 +16,48 @@ describe('suite ticket arrive topic consumer use case test', function () {
         }
         var Producer = kafka.Producer;
         producerClient = new kafka.Client(`${process.env.ZOOKEEPER_SERVICE_HOST}:${process.env.ZOOKEEPER_SERVICE_PORT}`);
-        var argv = {
-            topic: "/suite-ticket/arrive"
-        };
-        var topic = argv.topic || 'topic1';
-        var p = argv.p || 0;
-        var a = argv.a || 0;
-        producer = new Producer(producerClient, {requireAcks: 1});
+        producer = new Producer(producerClient);
         producer.on('ready', function () {
-            var args = {
-                suiteID: "suiteID",
-                ticket: "ticket",
-                dateTime: new Date()
-            };
-            producer.send([{
-                topic: topic,
-                messages: [JSON.stringify(args)]
-            }], function (err, result) {
-                done();
+            producer.createTopics(['suite-ticket-arrive'], true, (err, data)=> {
+                producerClient.refreshMetadata(['suite-ticket-arrive'], ()=> {
+                    done();
+                });
             });
         });
         producer.on('error', (err)=> {
             done(err);
         });
+        consumer = new SuiteTicketArriveTopicConsumer();
     });
     describe('#startConsume()', function () {
-        context('start consume /suite-ticket/arrive topic', function () {
-            it('should call suiteAccessTokenService.updateSuiteTicket methods when consumer this topic', function () {
-                var KConsumer = kafka.Consumer;
-                var client = new kafka.Client(`${process.env.ZOOKEEPER_SERVICE_HOST}:${process.env.ZOOKEEPER_SERVICE_PORT}`);
-                var topics = [{
-                    topic: "/suite-ticket/arrive",
-                    partition: 0,
-                    offset: 8000
-                }];
-                var options = {
-                    groupId: "suite-access-token-manage-group"
+        context('start consume suite-ticket-arrive topic', function () {
+            it('should call suiteAccessTokenService.updateSuiteTicket methods when consumer this topic', function (done) {
+                var mockSuiteAccessTokenService = {};
+                mockSuiteAccessTokenService.updateSuiteTicket = ()=> {
+                    done();
                 };
-                var consumer = new KConsumer(client, topics, options);
-                var self = this;
-                consumer.on('message', function (message) {
-                    console.log(message);
+                muk(consumer, "suiteAccessTokenService", mockSuiteAccessTokenService);
+                consumer.startConsume();
+                var suiteTicketData = {
+                    suiteID: "suiteID",
+                    ticket: "ticket",
+                    dateTime: new Date()
+                };
+                producer.send([{
+                    topic: "suite-ticket-arrive",
+                    messages: [JSON.stringify(suiteTicketData)]
+                }], ()=> {
                 });
             });
         });
     });
     after(function (done) {
-
         if (!process.env.IS_CI) {
             delete process.env.ZOOKEEPER_SERVICE_HOST;
             delete process.env.ZOOKEEPER_SERVICE_PORT;
         }
+        consumer.stopConsume();
+        producer.close();
         producerClient.close(()=> {
             done();
         });
